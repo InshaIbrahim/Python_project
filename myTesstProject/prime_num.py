a98c715d36e6a6ae5ffd4137d70d9f0ad5c77dc5
Insha <insha1983@gmail.com>
2024-07-09 15:58:42 +0300

    biggest number

---
+++ myTesstProject/prime_num.py
@@ -0,0 +1,11 @@
+num = int(input("entere a number"))
+if num == 1:
+    print("it is not prime number")
+if num > 1:
+    for i in range(2,num):
+        if num % i == 0:
+            print("it is not prime number")
+            
+            break
+    else:
+        print("it is a prime number")            
---
+++ myTesstProject/test1.py
@@ -0,0 +1,4 @@
+num=7
+
+for i in range(2,num):
+    print(i)
